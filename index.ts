ab3ebbe3db04d35e9baefc9605a6c6a6279e4bd4
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-12 10:50:25 +0700

    basic route + setup pug for views

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("ok")
+import express, { Express, Request, Response } from "express"; 
+
+const app: Express = express(); 
+const port: number = 3000;
+
+app.set('views', `${__dirname}/views`); // Tìm đến thư mục tên là views / Sử dụng trực tiếp biến dirname để môi trường deploy hiểu 
+app.set('view engine', 'pug'); // template engine sử dụng: pug
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.render("client/pages/topics/index");
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
