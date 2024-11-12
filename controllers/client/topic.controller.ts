2e026fc7b81435cacf89003c670491bada5c90ad
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-12 12:11:18 +0700

    FE client topic page

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -5,9 +5,9 @@ export const index = async (req: Request, res: Response) => {
   const topics = await Topic.find({
     deleted: false
   });
-  console.log(topics);
 
   res.render("client/pages/topics/index", {
-    pageTitle: "Chủ đề bài hát"
+    pageTitle: "Chủ đề bài hát",
+    topics: topics
   });
 }
