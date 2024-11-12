1c36f5845b77d36f443f0542fbe47100e48bf594
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-12 11:08:36 +0700

    connect database + take topics

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
-import express, { Express, Request, Response } from "express"; 
+import express, { Express, Request, Response } from "express";
 
-const app: Express = express(); 
+import dotenv from "dotenv";
+dotenv.config();
+import * as database from "./config/database";
+database.connect();
+
+import { Topic } from "./models/topic.model";
+
+const app: Express = express();
 const port: number = 3000;
 
 app.set('views', `${__dirname}/views`); // Tìm đến thư mục tên là views / Sử dụng trực tiếp biến dirname để môi trường deploy hiểu 
 app.set('view engine', 'pug'); // template engine sử dụng: pug
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false
+    });
+    console.log(topics);
+
     res.render("client/pages/topics/index");
 })
 
