6034ae63450999d383f13f037a1a095206a68883
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-13 22:13:56 +0700

    list songs

---
+++ routes/client/song.route.ts
@@ -0,0 +1,7 @@
+import express from "express";
+const router = express.Router();
+import * as controller from "../../controllers/client/song.controller";
+
+router.get("/:slugTopic", controller.index);
+
+export const songsRoute = router;
