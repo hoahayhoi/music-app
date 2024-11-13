40b47ba223b0d4ba92f4d1fde30c7c79c6825419
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-13 22:18:41 +0700

    song detail

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -10,11 +10,13 @@ export const index = async (req: Request, res: Response) => {
     deleted: false,
     status: "active"
   });
+  
   const songs = await Song.find({
     topicId: topic.id,
     deleted: false,
     status: "active"
   }).select("id title avatar singerId like slug");
+
   for (const song of songs) {
     const infoSinger = await Singer.findOne({
       _id: song.singerId,
@@ -22,8 +24,34 @@ export const index = async (req: Request, res: Response) => {
     });
     song["singerFullName"] = infoSinger ? infoSinger.fullName : "";
   }
+
   res.render("client/pages/songs/index", {
     pageTitle: topic.title,
     songs: songs
+  });
+}
+
+
+export const detail = async (req: Request, res: Response) => {
+  const slugSong = req.params.slugSong;
+  const song = await Song.findOne({
+    slug: slugSong,
+    deleted: false,
+    status: "active"
+  });
+
+  const singer = await Singer.findOne({
+    _id: song.singerId
+  }).select("fullName");
+
+  const topic = await Topic.findOne({
+    _id: song.topicId
+  }).select("title");
+
+  res.render("client/pages/songs/detail", {
+    pageTitle: "Chi tiết bài hát",
+    song: song,
+    singer: singer,
+    topic: topic
   });
 }
