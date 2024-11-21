a8e79e8d8d0fa67715ef152b8d84f7cc94180b44
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-21 16:23:40 +0700

    admin: topic page

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,9 +1,12 @@
 import { Express } from "express";
 import { systemConfig } from "../../config/system";
 import { dashboardRoute } from "./dashboard.route";
+import { topicRoutes } from "./topic.route";
 
 export const routesAdmin = (app: Express) => { 
     const path = systemConfig.prefixAdmin;
 
-    app.use(`/${path}/dashboard`, dashboardRoute)
+    app.use(`/${path}/dashboard`, dashboardRoute);
+
+    app.use(`/${path}/topics`, topicRoutes);
 }
