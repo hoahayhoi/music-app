84ab8f5756103f68c6b1bf3957020fadcdd56243
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-21 17:00:57 +0700

    admin: create song

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 import { Song } from "../../models/song.model";
+import { Topic } from "../../models/topic.model";
+import { Singer } from "../../models/singer.model";
+import { systemConfig } from "../../config/system";
+
 export const index = async (req: Request, res: Response) => {
   const songs = await Song.find({
     deleted: false
@@ -9,3 +13,25 @@ export const index = async (req: Request, res: Response) => {
     songs: songs
   });
 };
+
+export const create = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  }).select("title");
+
+  const singers = await Singer.find({
+    deleted: false
+  }).select("fullName");
+
+  res.render("admin/pages/songs/create", {
+    pageTitle: "Thêm mới bài hát",
+    topics: topics,
+    singers: singers
+  });
+};
+
+export const createPost = async (req: Request, res: Response) => {
+  const song = new Song(req.body);
+  await song.save();
+  res.redirect(`/${systemConfig.prefixAdmin}/songs`);
+}
--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -1,7 +1,21 @@
 import { Router } from "express";
 const router: Router = Router();
 import * as controller from "../../controllers/admin/song.controller";
+import multer from "multer"
+
+import { uploadSingle } from "../../middelwares/admin/uploadCloud.middleware";
+
+const upload = multer();
 
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+    "/create",
+    upload.single("avatar"),
+    uploadSingle,
+    controller.createPost
+);
 
 export const songRoutes: Router = router;
